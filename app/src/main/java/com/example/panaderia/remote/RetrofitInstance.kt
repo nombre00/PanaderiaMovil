be94d7693b4f68ecd6da109ddf9d102993a46890
nombre00 <leo.salgado@duocuc.cl>
2025-12-03 14:41:37 -0300

    version final

--- app/src/main/java/com/example/panaderia/remote/RetrofitInstance.kt
+++ app/src/main/java/com/example/panaderia/remote/RetrofitInstance.kt
@@ -7,7 +7,7 @@ object RetrofitInstance {
     // Se instancia el servicio de la API una sola vez
     val api: ApiService by lazy {
         Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:8080/") // URL base de la api, para conectarse desde el emulador al puerto del computador
+            .baseUrl("http://100.29.250.61:8080/") // URL base de la api, para conectarse desde el emulador al puerto del computador: "http://10.0.2.2:8080/"
             .addConverterFactory(GsonConverterFactory.create()) // Conversor de json
             .build()
             .create(ApiService::class.java) // Implementa la interfaz ApiService
